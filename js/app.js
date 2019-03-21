eeb1c28436dd96aee02ffd0d5a4a238c5165ccba
Luke Logan <llogan382@gmail.com>
2019-03-21 05:59:30 -0400

    adds element using jquery

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+// Create a button
+const $button = $('<button>Reveal Spoiler</button>');
+// Append adds an element as the last child element.
+$('.spoiler').append($button);
+// We want to append it to the last element of the .spoiler
+
+
+
 // Hide the spoiler text
 $('.spoiler span').hide();
 // When button is pressed
