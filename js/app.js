dd74318c2c62aa7b53d266a952dd8bdad1ae48ed
Luke Logan <llogan382@gmail.com>
2019-03-21 06:13:26 -0400

    add elements with event delegation

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
+
+// Within the parent selector, if the button tag is clicked, 
+// run this function
+$('.spoiler').on('click', 'button', function(){
+    $('.spoiler span').show();
+    $('.spoiler button').hide();
+});
+
 // Create a button
 const $button = $('<button>Reveal Spoiler</button>');
 // Append adds an element as the last child element.
 $('.spoiler').append($button);
 // We want to append it to the last element of the .spoiler
 
-
-
-// Hide the spoiler text
 $('.spoiler span').hide();
-// When button is pressed
-$('.spoiler button').click(function(){
-    $('.spoiler span').show();
-    $('.spoiler button').hide();
 
-});
 
     // Show the spoiler text
 
